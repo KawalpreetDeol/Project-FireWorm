23a36622eaa0b7c777ab78f1d0484acf83b16dd3
Unknown <s6vij@edu.uwaterloo.ca>
2018-01-13 18:42:06 -0500

    Added initial AngularJS files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectFireWormApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the projectFireWormApp
+ */
+angular.module('projectFireWormApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

7509e41c904d205e7dd03f3afae0b5f3419fb01d
Unknown <s6vij@edu.uwaterloo.ca>
2018-01-13 21:32:49 -0500

    Rewrote component scheme (TEST)

---
+++ src/app/app.contentcomponent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contentcomponent',
+  templateUrl: './app.contentcomponent.html',
+  styleUrls: ['./app.contentcomponent.css']
+})
+
+export class AppComponent {
+  title = 'Project FireWorm';
+}

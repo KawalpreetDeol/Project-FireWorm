de3ef74fd87e9ae57e02222fbf4258f70425f9ea
Unknown <s6vij@edu.uwaterloo.ca>
2018-01-13 21:13:22 -0500

    Even more errors?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.homecomponent.html',
-  styleUrls: ['./app.homecomponent.css']
-})
-
-@Component({
   selector: 'app-menubar',
   templateUrl: './app.menucomponent.html',
   styleUrls: ['./app.menucomponent.css']
+})
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './app.homecomponent.html',
+  styleUrls: ['./app.homecomponent.css']
 })
 
 export class AppComponent {

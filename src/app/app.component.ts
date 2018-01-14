2730cf8560f75499c1508d34f54b1861e97d1f53
Unknown <s6vij@edu.uwaterloo.ca>
2018-01-13 20:48:29 -0500

    Menubar testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.homecomponent.html',
+  styleUrls: ['./app.homecomponent.css']
 })
+
+@Component({
+  selector: 'app-menubar',
+  templateUrl: './app.menucomponent.html',
+  styleUrls: ['./app.menucomponent.css']
+})
+
 export class AppComponent {
   title = 'Project FireWorm';
 }
